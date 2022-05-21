1b2d5fad8dd1cc2b5c3ab8e5750a62fad58410b0
Ryan Lonergan <rlonergan@linode.com>
2022-05-21 17:13:18 -0400

    Updated versions.tf for all modules to support terraform v1.2.0

--- versions.tf
+++ versions.tf
@@ -1,11 +1,13 @@
 terraform {
+  required_version = ">= 0.13"
   required_providers {
     linode = {
       source = "linode/linode"
       # version = "..."
     }
     kubectl = {
-      source = "gavinbunney/kubectl"
+      source  = "gavinbunney/kubectl"
+      version = ">= 1.7.0"
     }
   }
 }
